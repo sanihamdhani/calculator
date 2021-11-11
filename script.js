c83f76459549039d6680cb81de1ff34b9ff3ca10
sanihamdhani <76431818+sanihamdhani@users.noreply.github.com>
2021-11-11 18:05:57 +0700

    update js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-function view(val) {
+const view = (val) => {
   document.getElementById("viewer").value += val;
-}
+};
 
-function clean() {
+const clean = () => {
   document.getElementById("viewer").value = "";
-}
+};
 
-function equal() {
+const equal = () => {
   var val = document.getElementById("viewer").value;
   if (val) {
     document.getElementById("viewer").value = eval(val);
   }
-}
-function sin() {
+};
+const sin = () => {
   var val = document.getElementById("viewer").value;
   if (val) {
     document.getElementById("viewer").value = Math.sin(val);
   }
-}
+};
 
-function cos() {
+const cos = () => {
   var val = document.getElementById("viewer").value;
   if (val) {
     document.getElementById("viewer").value = Math.cos(val);
   }
-}
+};
 
-function tan() {
+const tan = () => {
   var val = document.getElementById("viewer").value;
   if (val) {
     document.getElementById("viewer").value = Math.tan(val);
   }
-}
+};
 
-function persentase() {
+const persentase = () => {
   var val = document.getElementById("viewer").value;
   if (val) {
     document.getElementById("viewer").value = val / 100;
   }
-}
+};
